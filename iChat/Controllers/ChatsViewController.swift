295256ce4832ea00cbfb13290a937e7e0a447e46
s4rv4d <sarvadshetty@gmail.com>
2018-12-18 21:45:07 +0530

    configured recent chat cell

--- iChat/Controllers/ChatsViewController.swift
+++ iChat/Controllers/ChatsViewController.swift
@@ -7,13 +7,26 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ChatsViewController: UIViewController {
-
+    
+    //MARK:Variables
+    var recentChats:[NSDictionary] = []
+    var filterChats:[NSDictionary] = []
+    var recentListener:ListenerRegistration!
+    
+    //MARK:IBOutlets
+    @IBOutlet weak var recentChatTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //navigation bar setup
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        //tableview stuff
+        TableviewDelegateSetup()
+        LoadRecentChats()
     }
 
     //MARK:IBActions
@@ -23,4 +36,44 @@ class ChatsViewController: UIViewController {
         self.navigationController?.pushViewController(userViewController, animated: true)
     }
     
+}
+
+extension ChatsViewController:UITableViewDelegate,UITableViewDataSource{
+    func TableviewDelegateSetup(){
+        recentChatTableView.delegate = self
+        recentChatTableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("we have \(recentChats.count) recents")
+        return recentChats.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = recentChatTableView.dequeueReusableCell(withIdentifier: "recentCell", for: indexPath) as! RecentChatTableViewCell
+        //configuring cell
+        let recent = self.recentChats[indexPath.row]
+        cell.CellGenerate(recentChat: recent, index: indexPath)
+        return cell
+    }
+    
+    //MARK:LOAD
+    func LoadRecentChats(){
+        //to listen to only recent updates
+        recentListener = reference(.Recent).whereField(kUSERID, isEqualTo: FUser.currentId()).addSnapshotListener({ (snapshot, error) in
+            guard let snapshot = snapshot else {return}
+            self.recentChats = []
+            if !snapshot.isEmpty{
+                //get all snapshots,convert into dictionary and put in array and sort it using NSSortDescriptor
+                let sorted = ((dictionaryFromSnapshots(snapshots: snapshot.documents)) as NSArray).sortedArray(using: [NSSortDescriptor(key: kDATE, ascending: false)]) as! [NSDictionary]
+                //parse through all messages
+                for recent in sorted{
+                    if recent[kLASTMESSAGE] as! String != "" && recent[kCHATROOMID] != nil && recent[kRECENTID] != nil{
+                        self.recentChats.append(recent)
+                    }
+                }
+                self.recentChatTableView.reloadData()
+            }
+        })
+    }
 }
--- iChat/Controllers/Custom Cells/RecentChatTableViewCell.swift
+++ iChat/Controllers/Custom Cells/RecentChatTableViewCell.swift
@@ -8,17 +8,84 @@
 
 import UIKit
 
+//MARK:Protocol
+protocol RecentChatTableViewCellDelegate {
+    func ProfilePicTapped(index:IndexPath)
+}
+
 class RecentChatTableViewCell: UITableViewCell {
+    
+    //MARK:Variables
+    var indexPath:IndexPath!
+    let tapGestureRecog = UITapGestureRecognizer()
+    var delegate:RecentChatTableViewCellDelegate?
+    
+    //MARK:IBOutlets
+    @IBOutlet weak var profilePicImageview: UIImageView!
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var lastMessageLabel: UILabel!
+    @IBOutlet weak var messageCounterLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var messageCounterView: UIView!
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        messageCounterView.layer.cornerRadius = messageCounterView.frame.width/2
+        tapGestureRecog.addTarget(self, action: #selector(self.PicTap))
+        profilePicImageview.isUserInteractionEnabled = true
+        profilePicImageview.addGestureRecognizer(tapGestureRecog)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    //MARK:Cell Functions
+    func CellGenerate(recentChat:NSDictionary,index:IndexPath){
+        self.indexPath = index
+        //assigning values
+        self.fullNameLabel.text = recentChat[kWITHUSERFULLNAME] as? String
+        self.lastMessageLabel.text = recentChat[kLASTMESSAGE] as? String
+        self.messageCounterLabel.text = recentChat[kCOUNTER] as? String
+        
+        //for date
+        var date:Date!
+        if let created = recentChat[kDATE]{
+            if (created as! String).count != 14{
+                date = Date()
+            }else{
+                date = dateFormatter().date(from: created as! String)
+            }
+        }else{
+            date = Date()
+        }
+        self.dateLabel.text = timeElapsed(date: date)
+        //for avatar
+        if let avatarString = recentChat[kAVATAR]{
+            imageFromData(pictureData: avatarString as! String) { (image) in
+                if image != nil{
+                    self.profilePicImageview.image = image!.circleMasked
+                }
+            }
+        }
+        //for counter
+        if recentChat[kCOUNTER] as! Int != 0{
+            self.messageCounterLabel.text = "\(recentChat[kCOUNTER] as! Int)"
+            self.messageCounterLabel.isHidden = false
+            self.messageCounterLabel.isHidden = false
+        }else{
+            self.messageCounterLabel.isHidden = true
+            self.messageCounterLabel.isHidden = true
+        }
+    }
+    
+    @objc func PicTap(){
+     print("profile picture tapped at \(indexPath)")
+        delegate?.ProfilePicTapped(index: indexPath)
     }
 
 }
